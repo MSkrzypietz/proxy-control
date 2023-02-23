0208fc755072e5a501a7e5f5eae93b70fd7a233d
MSkrzypietz <michael.skr97@gmail.com>
2023-02-23 18:14:25 +0100

    Finish basic implementation for the proxy server

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,30 @@
 import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+const argv = yargs(hideBin(process.argv))
+  .option('target', {
+    alias: 't',
+    type: 'string',
+    description: 'Proxy target, e.g. {protocol}://{ip}:{port}',
+    demandOption: true
+  })
+  .parseSync();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+app.use('/controls', express.static('public'));
+
+app.use(
+  '/',
+  createProxyMiddleware({
+    target: argv.target,
+    changeOrigin: true
+  })
+);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
